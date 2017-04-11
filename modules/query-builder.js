0ff0cb2b50ce05ebe5bc2e8616f1a578a899f02a
Alexandru Olaru <aolaru@pentalog.fr>
2017-04-11 11:37:54 +0300

    Added the source code for the filter-extractor and query builder

---
+++ modules/query-builder.js
@@ -0,0 +1,33 @@
+function isDate(value) {
+  return /^[0-9]{4}-[0-9]{2}-[0-9]{2}$/gi.test(value);
+}
+
+function buildOperatorValue(filter) {
+  const operator = filter.operator;
+  let value = filter.value;
+  let result = {};
+
+  if (isDate(value)) {
+    value = new Date(value);
+  }
+
+  if (operator !== null) {
+    result[operator] = value;
+  } else {
+    result = value;
+  }
+
+  return result;
+}
+
+
+module.exports.build = (filters) => {
+  if (!filters) return {};
+
+  return filters.reduce((acc, filter) => {
+    acc[filter.property] = buildOperatorValue(filter);
+
+    return acc;
+  }, {});
+};
+
---
+++ test/query-builder.spec.js
@@ -0,0 +1,30 @@
+/* global describe it */
+const qb = require('../index').queryBuilder;
+const assert = require('assert');
+
+const configs = [
+  {
+    in: [{
+      property: 'created_at',
+      value: '2016-09-15',
+      operator: '$gt',
+    }],
+    out: {
+      created_at: {
+        $gt: new Date('2016-09-15'),
+      },
+    },
+  },
+  {
+    in: null,
+    out: {},
+  },
+];
+
+describe('Query Builder', () => {
+  configs.forEach((config) => {
+    it(`should return ${JSON.stringify(config.out)} given ${JSON.stringify(config.in)}`, () => {
+      assert.deepEqual(qb.build(config.in), config.out);
+    });
+  });
+});
